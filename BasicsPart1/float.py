af15167b3d61f810bb0f0f544934e244bf1fc7ca
Mark Gatere <gateremark5@gmail.com>
2022-07-02 09:10:11 +0300

    Added a new file

---
+++ BasicsPart1/float.py
@@ -0,0 +1,11 @@
+# These are numbers with a decimal point; 5.7, 8.0, 3.5554, 0.00003, -0.543, -4.229.
+
+print(2 / 4)
+
+print(type(2.7))
+
+print(type(-27.554))
+
+print(type(2.8 + 4))
+
+print(type(2 / 4))
