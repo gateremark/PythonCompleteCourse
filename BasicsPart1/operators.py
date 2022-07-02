69a24f92930d5e07f7f7f92cf356f93d6123cb9f
Mark Gatere <gateremark5@gmail.com>
2022-07-02 09:17:18 +0300

    Added a new file

---
+++ BasicsPart1/operators.py
@@ -0,0 +1,33 @@
+"""
+operators + , - , * , / , ** , // , %
+
++ ----- addition
+- ----- subtraction
+* ----- multiplication
+/ ----- division
+** ---- 'power of'
+// ---- 'rounds down the quotient to the nearest whole number'
+% ----- 'returns the remainder'
+"""
+
+print(2 ** 4)
+
+print(7 ** 9)
+
+print(2 // 4)
+
+print(3 // 4)
+
+print(4 // 4)
+
+print(5 // 4)
+
+print(7 // 4)
+
+print(9 // 4)
+
+print(5 % 4)
+
+print(5 % 2)
+
+print(5 % 3)
