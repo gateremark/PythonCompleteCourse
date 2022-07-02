57f1085ef5ab16ada453fe58da3921f1dc972971
Mark Gatere <gateremark5@gmail.com>
2022-07-02 09:07:59 +0300

    Added a new file

---
+++ BasicsPart1/integer.py
@@ -0,0 +1,15 @@
+# An integer is a whole number with no decimal point; 1, 2, 3, 4, 456, -4643, -77.
+
+print(2 + 4)
+
+print(2 - 4)
+
+print(2 * 4)
+
+print(type(2))
+
+print((type(-29)))
+
+print(type(2 + 4))
+
+print(type(2 - 4))
