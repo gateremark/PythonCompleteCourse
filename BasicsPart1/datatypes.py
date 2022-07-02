b9c8acc6e95f92dbcc15042da5941616d9366767
Mark Gatere <gateremark5@gmail.com>
2022-07-02 09:04:24 +0300

    Added new files

---
+++ BasicsPart1/datatypes.py
@@ -0,0 +1,29 @@
+"""
+-Text Type:
+str - string
+
+-Numeric Types:
+int - integer
+float
+complex
+
+-Sequence Types:
+list
+tuple
+range
+
+-Mapping Type:
+dict - dictionary
+
+-Set Types:
+set
+frozenset
+
+-Boolean Type:
+bool - boolean
+
+-Binary Types:
+bytes
+bytearray
+memoryview
+"""
---
+++ BasicsPart1/formattedStrings.py
@@ -0,0 +1,5 @@
+name = 'Mark'
+age = 20
+print("Hi " + name + ". You are " + str(age) + " years old.")
+# or we can use formatted strings
+print(f"Hi {name}. You are {age} years old.")
