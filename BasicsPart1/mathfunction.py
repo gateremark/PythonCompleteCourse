424d8dc7728f2beffeaec708afd5c922abcee08d
Mark Gatere <gateremark5@gmail.com>
2022-07-02 09:24:42 +0300

    Added a new file and edited the operators file

---
+++ BasicsPart1/mathfunction.py
@@ -0,0 +1,25 @@
+#Math Functions (Actions performed on integers and floats)
+
+print(round(3.1))
+print(round(3.5))
+print(abs(0))
+print(abs(-3.1))
+print(abs(3.1))
+print(pow(2,3))
+print(pow(4,3))
+print(max(2,3))
+print(max(2,-9))
+print(min(3,8))
+print(min(2,-9))
+
+#For most of the other mathematical functions to be used, they have to be 'imported' from the math module
+
+from math import *
+print(floor(3.7))
+print(floor(5.9))
+print(ceil(3.2))
+print(ceil(5.9))
+print(ceil(5.3))
+print(sqrt(4))
+print(sqrt(9))
+print(sqrt(121))
--- BasicsPart1/operators.py
+++ BasicsPart1/operators.py
@@ -9,25 +9,18 @@ operators + , - , * , / , ** , // , %
 // ---- 'rounds down the quotient to the nearest whole number'
 % ----- 'returns the remainder'
 """
-
+print(1+6)
+print(5-2)
+print(9/3)
+print(7*7)
 print(2 ** 4)
-
 print(7 ** 9)
-
 print(2 // 4)
-
 print(3 // 4)
-
 print(4 // 4)
-
 print(5 // 4)
-
 print(7 // 4)
-
 print(9 // 4)
-
 print(5 % 4)
-
 print(5 % 2)
-
 print(5 % 3)
