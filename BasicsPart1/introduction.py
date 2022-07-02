208c6ad4244ae8146c5b3500e2813ad4cf5b8cee
Mark Gatere <gateremark5@gmail.com>
2022-07-02 08:57:29 +0300

    Included an introduction file

---
+++ BasicsPart1/introduction.py
@@ -0,0 +1,30 @@
+"""
+Introduction
+
+What is programming?
+In simple terms, programming is a way for us to give instructions to computers. "Giving it an instruction manual, and the computer follows".
+
+Computers don't understand English or any other human language for that matter. Computers speak in 1s and 0s; On or Off (So is all the other electronic component).
+But for humans, it would be gibberish or rather difficult to communicate in 1s or 0s. Humans have then developed programming languages that work in between human language and machine language (0s / 1s).
+Some programming languages are low-level (closer to machine language) while others are high-level (closer to human language).
+Examples of low-level programming languages include:
+-Assembly
+
+Examples of high-level programming languages include:
+-Python
+-JavaScript
+
+What then is a programming language?
+A programming language is any set of rules that convert strings, or graphical program elements, to various kinds of machine code output (that is understandable to the computer).
+
+Note: At the end of the day, all programming languages do the same thing, they tell the machine what to do. However, different languages have different modes of doing it.
+The beauty is that "most languages have very similar principles."
+
+The hardest part is only in learning the first language.
+
+In between the programming language and the machine language, we need a 'translator' that understands both the programming language (source code) and machine language and hence can translate our code to machine language for the computers to understand.
+*The translator ain't a person but rather another program that can either be an interpreter or a compiler.
+Like a translator, an interpreter goes line by line throughout the code and executes the code on the machine. In contrast, a compiler takes the code and reads the entire file all at once and translates it to machine code. (Their other major differences are a little complicated and beyond the scope of the course)
+Python usually uses an interpreter hence to use python you have to download the interpreter.
+
+"""
